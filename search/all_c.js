169ad9e9e26a9fa3edca042b1495bb773ebec633
Cyniadis <Cyniadis@users.noreply.github.com>
2023-11-05 18:22:11 +0000

    deploy: 4148ceea6f410bfc7a9af99f73d88725e8d8de7b

---
+++ search/all_c.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['saveallsubtreewords_0',['saveAllSubtreeWords',['../class_tree_search.html#aeaf53b5d98986dde556c5a515c5f3cc2',1,'TreeSearch']]],
+  ['saveword_1',['saveWord',['../class_tree_search.html#a335809928ac538a215e2e31c165ae68c',1,'TreeSearch']]],
+  ['searchinterface_2',['SearchInterface',['../class_search_interface.html',1,'']]],
+  ['searchwords_3',['searchwords',['../class_naive_search.html#acc8ebdd17974045cd86a29d494b555a3',1,'NaiveSearch::searchWords(const std::string &amp;subStr) override'],['../class_naive_search.html#a3809311c76a02c705ed38fb00d105a7c',1,'NaiveSearch::searchWords(const std::string &amp;subStr, StringVecConstIterator startIt, StringVecConstIterator endIt)'],['../class_naive_search_parallel.html#aa6fa391330a56ce0430c5a2139aca37c',1,'NaiveSearchParallel::searchWords()'],['../class_search_interface.html#a7f2fd4a4b6b4b2506ead5b4e957d0917',1,'SearchInterface::searchWords()'],['../class_tree_search.html#a5f5bdbcb0bbf2ab89eea8139a635302e',1,'TreeSearch::searchWords(const std::string &amp;subStr) override'],['../class_tree_search.html#a8edee9e943353f8f8757ca1521fce629',1,'TreeSearch::searchWords(const std::string &amp;subStr, std::shared_ptr&lt; TreeNodeBase &gt; &amp;treeNode)']]],
+  ['searchwordtimed_4',['searchWordTimed',['../class_search_interface.html#a29019f8d881b09697dcd10095bc4173b',1,'SearchInterface']]],
+  ['setword_5',['setWord',['../class_tree_node_base.html#a4e659dfec1dc0e346f94d514ce814943',1,'TreeNodeBase']]],
+  ['spinlock_6',['SpinLock',['../class_spin_lock.html',1,'']]],
+  ['startstwith_7',['startstWith',['../class_naive_search.html#af69407246995c28a6b34169ada30013b',1,'NaiveSearch']]]
+];
